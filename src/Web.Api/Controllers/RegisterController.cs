56a75114fc13d8b002d8152ee87ff057edc0aedc
Hong <procyonx@member.sdu.ac.kr>
2019-10-01 16:56:11 +0900

    회원 가입을 하던 AccountController를 RegisterController로 변경.

--- src/Web.Api/Controllers/RegisterController.cs
+++ src/Web.Api/Controllers/RegisterController.cs
@@ -1,4 +1,8 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Web.Api.Core.Dto.UseCaseRequests;
 using Web.Api.Core.Interfaces.UseCases;
@@ -8,26 +12,27 @@ namespace Web.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class AccountsController : ControllerBase
+    public class RegisterController : ControllerBase
     {
         private readonly IRegisterUserUseCase _registerUserUseCase;
         private readonly RegisterUserPresenter _registerUserPresenter;
 
-        public AccountsController(IRegisterUserUseCase registerUserUseCase, RegisterUserPresenter registerUserPresenter)
+        public RegisterController(IRegisterUserUseCase registerUserUseCase, RegisterUserPresenter registerUserPresenter)
         {
             _registerUserUseCase = registerUserUseCase;
             _registerUserPresenter = registerUserPresenter;
         }
 
-        // POST api/accounts
         [HttpPost]
         public async Task<ActionResult> Post([FromBody] Models.Request.RegisterUserRequest request)
         {
-            if (!ModelState.IsValid)
+            if(!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
             }
+
             await _registerUserUseCase.Handle(new RegisterUserRequest(request.FirstName, request.LastName, request.Email, request.UserName, request.Password), _registerUserPresenter);
+
             return _registerUserPresenter.ContentResult;
         }
     }
